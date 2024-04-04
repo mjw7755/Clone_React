57f1aa92bd847041a812e39f855a5663a6882458
문재웅 <mjw7755@naver.com>
2024-04-04 16:50:30 +0900

    initial commit

---
+++ src/main/java/org/com/clone_react/CloneReactApplication.java
@@ -0,0 +1,13 @@
+package org.com.clone_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneReactApplication.class, args);
+    }
+
+}
